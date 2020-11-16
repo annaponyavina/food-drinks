6b1a01156700446de8090db7ddde96f445142119
Anna Ponyavina <ponyavina1997@mail.ru>
2020-11-17 00:03:14 +0300

    create: Header and NavBar

---
+++ src/сomponents/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './NavBar.scss'
+
+export const NavBar = () => {
+  return (
+      <nav className="nav">
+        <div className="container">
+          <div className="nav__content">
+            <a href="#" className="nav__item">Home</a>
+            <a href="#" className="nav__item">Vintage</a>
+            <a href="#" className="nav__item">Imperial</a>
+            <a href="#" className="nav__item">Sparkling</a>
+            <a href="#" className="nav__item">Accessories</a>
+            <a href="#" className="nav__item">Block</a>
+            <a href="#" className="nav__item">Product Details</a>
+            <a href="#" className="nav__item">Contact</a>
+          </div>
+        </div>
+      </nav>
+  )
+}
